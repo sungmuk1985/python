020248e6ff5209f9464aca9e90cea9d0e361230a
sungmuk1985 <sungmuk1985@gmail.com>
2020-02-10 23:57:36 +0900

    ㄴㄹ

---
+++ python/index.py
@@ -0,0 +1,33 @@
+#!C:\Users\sungm\AppData\Local\Programs\Python\Python38\Python.exe
+#!/usr/local/bin/python3
+노성
+print("Content-Type: text/html")
+print()
+import cgi
+form = cgi.FieldStorage()
+if 'id' in form:
+    pageId = form["id"].value
+    description = open('data/'+pageId, 'r').read()
+else:
+    pageId = 'Welcome'
+    description = 'Hello, web'
+print('''<!doctype html>
+<html>
+<head>
+    <body background='img/muk.png'
+  <title>This is Python Test WEB</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1><a href="index.py">WEB</a></h1>
+  <ol>
+    <li><a href="index.py?id=HTML">HTML</a></li>
+    <li><a href="index.py?id=CSS">CSS</a></li>
+    <li><a href="index.py?id=JavaScript">JavaScript</a></li>
+    <li><a href="index.py?id=Python">Python</a></li>
+  </ol>
+  <h2>{title}</h2>
+  <p>{desc}</p>
+</body>
+</html>
+'''.format(title=pageId, desc=description))
